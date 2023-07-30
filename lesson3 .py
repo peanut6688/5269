fc5affbe75f2c3dce9e40211d90a613373ee54ce
peanut6688 <140928139+peanut6688@users.noreply.github.com>
2023-07-30 04:26:30 +0000

    bvkgkk

--- lesson3 .py
+++ lesson3 .py
@@ -1,1 +1,6 @@
-print("老二")
+#讓使用者輸入梯形的上底、下底及高，程式會計算梯形的面積(上底加下底乘以高除以2)
+top = eval(input("請輸入梯形的上底(公分):"))
+bottom = eval(input("請輸入梯形的下底(公分):"))
+height = eval(input("請輸入梯形的高(公分):"))
+area = (top + bottom) * height / 2
+print("梯形的面積:",area,"平方公分")
